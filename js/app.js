3073240c9d6f63ca0178df62b690d6329faa80bf
CHRISTINE LORA <CHRISTINELORA23@GMAIL.COM>
2021-07-17 18:17:20 -0700

    Lab 3 additions to my about me page

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+let correctcount = 0;
 // ask the users name to greet them
 let username = prompt('What\'s your name?');
 
@@ -15,6 +15,7 @@ let answer0ne = prompt('Do you think I like sushi? Yes or no').toLowerCase();
 // response to the users answer:
 if (answer0ne === 'yes' || answer0ne === 'y') {
     alert('Yassss, you got that right! I LOVE sushi!');
+    correctcount++;
 } else if (answer0ne === 'no' || answer0ne === 'n') {
     alert('Sorry, you\'re incorrect');
 } else {
@@ -31,8 +32,9 @@ let dogsUpperCase = areDogsCute.toUpperCase();
 // console.log(dogsUpperCase);
 
 if (dogsUpperCase === 'YES' || dogsUpperCase === 'YAS') {
-  alert('Heck yeah, dogs are cute!');
-} else if (dogsUpperCase === 'NO' || dogsUpperCase === 'n') { 
+    alert('Heck yeah, dogs are cute!');
+    correctcount++;
+} else if (dogsUpperCase === 'NO' || dogsUpperCase === 'n') {
     alert('WOW, you couldn\'t be more wrong :(')
 }
 
@@ -44,6 +46,7 @@ let doWorkOut = prompt('Do I enjoy working out? Yes or no');
 // response to the users answer:
 if (doWorkOut === 'yes' || doWorkOut === 'y') {
     alert('You are absolutely right. Wow, you already know so much about me :)');
+    correctcount++;
 } else if (doWorkOut === 'no' || doWorkOut === 'n') {
     alert('It\'s like you don\'t know me at all')
 }
@@ -56,6 +59,7 @@ let plantMom = prompt('Am I a plant mom to 50+ house plants?');
 // response to users answer:
 if (plantMom === 'yes' || plantMom === 'y') {
     alert('You already know! Thank you for being correct');
+    correctcount++;
 } else if (plantMom === 'no' || plantMom === 'n') {
     alert('Aw c\'mon, try again!')
 }
@@ -67,16 +71,81 @@ let petDogs = prompt('Do I try to pet every day I come across?');
 
 if (petDogs === 'yes' || petDogs === 'y') {
     alert('Yep, I really do. You are correct');
+    correctcount++;
 } else if (petDogs === 'no' || petDogs === 'n') {
     alert('I\'m almost offended you would think no. Try again.')
 }
 
+// - Add a 6th question to the guessing game that takes in a numeric input by prompting the user to guess a number.
+// - Indicates through an alert if the guess is “too high” or “too low”.
+// - It should give the user exactly four opportunities to get the correct answer.
+// - After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of some sort.
 
+// QUESTION 6: 
+let myFavoriteNumber = 5;
+let usersGuess = +prompt('What is my favorite number? Hint: Less than 10, greater than 2. You have 4 tries to guess!');
 
+let attemptsRemaining = 3;
+let attempts = 3;
 
+while (attemptsRemaining) {
+    for (let i = 0; i < attempts; i++) {
+        if (usersGuess === myFavoriteNumber) {
+            attemptsRemaining = 0;
+            alert(`You're correct!`);
+            correctcount++;
+            break;
+        }
+        else if (usersGuess < myFavoriteNumber) {
+            alert(`That's too low, try again! You have ${attemptsRemaining} attempts remaining.`);
 
+            usersGuess = +prompt('Can you guess again?');
+        }
+        else if (usersGuess > myFavoriteNumber) {
+            alert(`That's too high, guess again! You have ${attemptsRemaining} attempts remaining.`);
+            usersGuess = +prompt(`Please try another number`);
+        }
+        attemptsRemaining--;
+        if (attemptsRemaining === 0 && usersGuess !== myFavoriteNumber) {
+            alert('The correct answer is ' + myFavoriteNumber);
+        }
+    }
+}
 
+// Add a 7th question that has multiple possible correct answers that are stored in an array.
+// Give the user 6 attempts to guess the correct answer.
+// The guesses will end once the user guesses a correct answer or they run out of attempts.
+// Display all the possible correct answers to the user.
+// Consider using a loop of some sort for this question.
 
+// QUESTION 7:
+let myFavoriteAnimals = ['cats', 'dogs', 'penguins', 'octopus'];
+let userInput = prompt('what is my favorite animal?');
+let attempts = 5;
+let guessedCorrectly = false;
 
+while (attempts) {
+    if (!attempts) {
+        alert(`You're out of attempts`);
+        break;
+    }
+    for (let i = 0; i < myFavoriteAnimals.length; i++) {
+        if (userInput === myFavoriteAnimals[i]) {
+            alert(`You got it right!`);
+            correctcount++;
+            guessedCorrectly = true;
+            break;
+        } 
+    } 
+    if (guessedCorrectly) {
+        correctcount++;
+        attempts = 0; 
+        break; 
+    }
+    alert(`Please try again, you have ${attempts} remaining`);
+        userInput = prompt(`What's my favorite animal?`);
+        attempts--;
+} 
+// Keep track of the total number of correct answers. At the end tell them how many they got correct out of the 7 questions asked.
 
-
+alert(`You scored ${score} out of 7 on my about me quiz!! YASSSS!`);
